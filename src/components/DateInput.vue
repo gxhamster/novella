570839e2771620162ed1c588b2305818528e5059
Iyaan Azeez <iyaanazeez757@gmail.com>
2022-02-27 11:19:15 +0500

    Added: Date picker

--- src/components/DateInput.vue
+++ src/components/DateInput.vue
@@ -2,10 +2,14 @@
   <div class="laptop:w-52 desktop:w-60">
     <label class="block mb-2 laptop:text-sm desktop:text-md" for="text input">{{ props.title }}</label>
     <div class="relative">
-      <input type="date" class="rounded-lgg bg-secondary text-gray-400 w-full border-steel border-2 desktop:py-2 desktop:px-4 laptop:py-1 laptop:px-3">
-      <div class="bg-primary rounded-full  inline-block top-0 right-0 absolute laptop:p-2 desktop:p-3">
-        <CalendarMonthOutline class="text-white"/>
-      </div>
+      <DatePicker color="blue">
+        <template v-slot="{ inputValue, togglePopover }">
+        <input readonly :value="inputValue" class="rounded-lgg bg-secondary text-gray-400 w-full border-steel border-2 desktop:py-2 desktop:px-4 laptop:py-1 laptop:px-3">
+        <div @click="togglePopover" class="bg-primary cursor-pointer rounded-full  inline-block top-0 right-0 absolute laptop:p-2 desktop:p-3">
+          <CalendarMonthOutline class="text-white"/>
+        </div>
+        </template>
+      </DatePicker>
     </div>
   </div>
 </template>
@@ -13,6 +17,7 @@
 <script setup>
 import { defineProps } from 'vue'
 import CalendarMonthOutline from 'vue-material-design-icons/CalendarMonthOutline.vue'
+import { DatePicker } from 'v-calendar'
 
 const props = defineProps({
   title: String
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import { router } from './router'
 import './main.css'
+import VCalendar from 'v-calendar';
+import 'v-calendar/dist/style.css';
 
-
-createApp(App).use(router).mount('#app')
+createApp(App).use(VCalendar).use(router).mount('#app')
