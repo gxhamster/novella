9a3b317920b43469afb4a7920d55a4d3cc1cc77e
Iyaan Azeez <iyaanazeez757@gmail.com>
2022-03-04 16:29:32 +0500

    Fixed: set max width on page content

--- src/components/PageContainer.vue
+++ src/components/PageContainer.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="desktop:p-20 laptop:py-10 laptop:px-14 bg-secondary rounded-lgg flex-grow overflow-hidden">
-    <span class="block desktop:text-3xl laptop:text-2xl font-medium desktop:mb-6 laptop:mb-3">{{ props.title }}</span>
-    <div class="main-container flex flex-col justify-between">
-      <slot></slot>
-    </div>
+    <section class="container">
+      <span class="block desktop:text-3xl laptop:text-2xl font-medium desktop:mb-6 laptop:mb-3">{{ props.title }}</span> <div class="main-container mx-auto flex flex-col justify-between">
+        <slot></slot>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -12,3 +13,11 @@ import { defineProps } from 'vue'
 
 const props = defineProps({ title: String })
 </script>
+
+<style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+  height: 100%;
+}
+</style>
