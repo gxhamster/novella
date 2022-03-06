86c0cb6aef7a490b6bea33086eb9e08160e84edc
Iyaan Azeez <iyaanazeez757@gmail.com>
2022-03-06 17:00:09 +0500

    Fixed: Move sidebar buttons higher

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-primary desktop:w-128 laptop:w-100 h-screen  justify-start flex flex-col items-center py-7">
     <SchoolLogo class="w-full desktop:px-8 laptop:px-8"/>
-    <div class="flex flex-col my-2 items-center w-full justify-center gap-5 desktop:mt-20 laptop:mt-10 px-2" >
+    <div class="flex flex-col my-2 items-center w-full justify-center gap-5 desktop:mt-12 laptop:mt-10 px-2" >
       <div class="w-full" v-for="(name, index) in button_names" :key="name.id">
         <router-link :to="route_names[index]">
         <SidebarButtons :title="name" @clicked="btnClicked(index)" :isActive="current_active_btn_index == index ? true : false">
