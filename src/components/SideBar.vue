4f9053ef68cb18f52964c03bbc03ce76b3de7251
Iyaan Azeez <iyaanazeez757@gmail.com>
2022-03-09 23:58:15 +0500

    feat(Sidebar): keyboard navigation in the sidebar buttons

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-primary desktop:w-128 laptop:w-100 h-screen  justify-start flex flex-col items-center py-7">
+  <div @mouseleave="setFocus(false)" @mouseover="setFocus(true)" @click="setFocus" class="bg-primary desktop:w-128 laptop:w-100 h-screen  justify-start flex flex-col items-center py-7">
     <SchoolLogo class="w-full desktop:px-8 laptop:px-8"/>
     <div class="flex flex-col my-2 items-center w-full justify-center gap-5 desktop:mt-12 laptop:mt-10 px-2" >
       <div class="w-full" v-for="(name, index) in button_names" :key="name.id">
@@ -18,19 +18,22 @@
 
 <script setup>
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 
 import SchoolLogo from './SchoolLogo.vue'
 import SidebarButtons from './SidebarButtons'
 import NovellaLogo from './NovellaLogo'
-import  AddNewBookIcon from './icons/AddNewBookIcon'
-import  AddNewStudentIcon from './icons/AddNewStudentIcon'
-import  DashboardIcon from './icons/DashboardIcon'
-import  HistoryIcon from './icons/HistoryIcon'
-import  IssueBookIcon from './icons/IssueBookIcon'
-import  ReturnBookIcon from './icons/ReturnBookIcon'
+import AddNewBookIcon from './icons/AddNewBookIcon'
+import AddNewStudentIcon from './icons/AddNewStudentIcon'
+import DashboardIcon from './icons/DashboardIcon'
+import HistoryIcon from './icons/HistoryIcon'
+import IssueBookIcon from './icons/IssueBookIcon'
+import ReturnBookIcon from './icons/ReturnBookIcon'
 
 import { routes } from '../router'
 
+const router = useRouter()
+const sidebar_focused = ref(false)
 const current_active_btn_index = ref(0)
 const btnClicked = (index) => {
     current_active_btn_index.value = index
@@ -38,6 +41,45 @@ const btnClicked = (index) => {
 const icon_names = [IssueBookIcon, ReturnBookIcon, AddNewBookIcon, AddNewStudentIcon, HistoryIcon, DashboardIcon]
 const button_names = ref(["Issue Book", "Receive Book", "Add New Book", "Add New Student", "History", "Dashboard"])
 const route_names =  ref(routes.filter((item) => item.path !== '/').map((item) => item.path))
+
+window.onkeyup = (event) => {
+  if (event.key === "ArrowUp") {
+    navigateSidebar(false)
+  } else if (event.key === "ArrowDown") {
+    navigateSidebar()
+  } else if (event.key === "Enter") {
+    changeRoute()
+  }
+}
+
+function wrap(value, lower, upper) {
+  if (value > upper) {
+    value = lower
+  } else if (value < lower) {
+    value = upper
+  }
+
+  return value
+}
+
+
+function changeRoute() {
+  router.push(route_names.value[current_active_btn_index.value])
+}
+
+function navigateSidebar(down = true) {
+  if (sidebar_focused.value) {
+    if (down)
+      current_active_btn_index.value = wrap(current_active_btn_index.value + 1, 0, button_names.value.length - 1)
+    else {
+      current_active_btn_index.value = wrap(current_active_btn_index.value - 1, 0, button_names.value.length - 1)
+    }
+  }
+}
+
+function setFocus(focus = true) {
+  sidebar_focused.value = focus
+}
 </script>
 
 <style>
